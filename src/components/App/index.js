a1225c0529a437039883404d87ff7a4d30c32f5b
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-09-01 14:32:02 +0300

    made website clearer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Header from "../Header";
 import Main from "../Main";
-import Footer from "../Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Main />
-      <Footer />
     </BrowserRouter>
   );
 };
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,14 +1,17 @@
 import Info from "../Info";
 import News from "../News";
 import Coaches from "../Coaches";
+import Footer from "../Footer";
+
 
 export default function Main() {
   return (
     <div className="container">
       <div className="main">
-        <Info></Info>
-        <News></News>
-        <Coaches></Coaches>
+        <Info />
+        <News />
+        <Coaches />
+        <Footer />
       </div>
     </div>
   )
